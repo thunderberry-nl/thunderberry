85958e53e92214fa2539b2f09a2fe2e9bc1e08ad
Hakky54 <hakangoudberg@hotmail.com>
2025-04-02 10:44:46 +0200

    allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+        "5d7a3d08-b142-4618-9b6d-82f78dbaf7d6.lovableproject.com"
+    ]
   },
   plugins: [
     react(),
