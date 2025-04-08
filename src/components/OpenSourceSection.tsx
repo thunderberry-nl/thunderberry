37f8652c4c6c2bd3a824f0667fdda3655f4176c6
Hakky54 <hakangoudberg@hotmail.com>
2025-04-09 01:34:49 +0200

    removed pr number

--- src/components/OpenSourceSection.tsx
+++ src/components/OpenSourceSection.tsx
@@ -9,7 +9,6 @@ type Contribution = {
   showProjectNameInRows: boolean
   contributions: {
     url: string;
-    prNumber: string;
     title?: string;
     projectName?: string;
   }[];
@@ -23,9 +22,9 @@ export default function OpenSourceSection() {
       logo: '/logo/elasticsearch.svg',
       showProjectNameInRows: false,
       contributions: [
-        { url: 'https://github.com/elastic/elasticsearch/pull/53533', prNumber: '53533', title: 'Enable support for decompression of compressed response within RestHighLevelClient' },
-        { url: 'https://github.com/elastic/elasticsearch/pull/55413', prNumber: '55413', title: 'Enable decompression of response within LowLevelRestClient' },
-        { url: 'https://github.com/elastic/elasticsearch/pull/67802', prNumber: '67802', title: 'Added support for passing http query parameters' },
+        { url: 'https://github.com/elastic/elasticsearch/pull/53533', title: 'Enable support for decompression of compressed response within RestHighLevelClient' },
+        { url: 'https://github.com/elastic/elasticsearch/pull/55413', title: 'Enable decompression of response within LowLevelRestClient' },
+        { url: 'https://github.com/elastic/elasticsearch/pull/67802', title: 'Added support for passing http query parameters' },
       ],
     },
     {
@@ -33,29 +32,29 @@ export default function OpenSourceSection() {
       logo: '/logo/tomcat.png',
       showProjectNameInRows: false,
       contributions: [
-        { url: 'https://github.com/apache/tomcat/pull/673', prNumber: '673', title: 'Added option to use custom SSLContext' },
-        { url: 'https://github.com/apache/tomcat/pull/805', prNumber: '805', title: 'Simplify usage of custom ssl configuration' },
+        { url: 'https://github.com/apache/tomcat/pull/673', title: 'Added option to use custom SSLContext' },
+        { url: 'https://github.com/apache/tomcat/pull/805', title: 'Simplify usage of custom ssl configuration' },
       ],
     },
     {
       project: 'Http4k',
       showProjectNameInRows: false,
       contributions: [
-        { url: 'https://github.com/http4k/http4k/pull/545', prNumber: '545', title: 'Bugfix: add nullsafety and fallback for description within the Status' },
-        { url: 'https://github.com/http4k/http4k/pull/547', prNumber: '547', title: 'Start ApacheAsyncClient by default if not started yet' },
+        { url: 'https://github.com/http4k/http4k/pull/545', title: 'Bugfix: add nullsafety and fallback for description within the Status' },
+        { url: 'https://github.com/http4k/http4k/pull/547', title: 'Start ApacheAsyncClient by default if not started yet' },
       ],
     },
     {
       project: 'HTTP Libraries',
       showProjectNameInRows: true,
       contributions: [
-        { url: 'https://github.com/Kong/unirest-java/pull/381', prNumber: '381', title: 'Support for custom ciphers and protocols for https requests', projectName: 'Unirest-Java' },
-        { url: 'https://github.com/Kong/unirest-java/pull/384', prNumber: '384', title: 'Removed redundant access modifiers, fixed typos and simplified some assertions', projectName: 'Unirest-Java' },
-        { url: 'https://github.com/eclipse-vertx/vert.x/pull/3719', prNumber: '3719', title: 'Support for generic Key and Trust material', projectName: 'Vert.x' },
-        { url: 'https://github.com/com-lihaoyi/requests-scala/pull/62', prNumber: '62', title: 'Support for custom SSLContext for ssl configuration', projectName: 'Requests-Scala' },
-        { url: 'https://github.com/ktorio/ktor/pull/1852', prNumber: '1852', title: 'Bug-fix: enhanced handling of statusCode for AndroidEngine', projectName: 'Ktor' },
-        { url: 'https://github.com/eclipse-ee4j/jersey/pull/4573', prNumber: '4573', title: 'Support for SSL Configuration within JerseyTest', projectName: 'Jersey' },
-        { url: 'https://github.com/JetBrains/Exposed/pull/1852', prNumber: '1852', title: 'Fix: Verbose logging in test module', projectName: 'Exposed' },
+        { url: 'https://github.com/Kong/unirest-java/pull/381', title: 'Support for custom ciphers and protocols for https requests', projectName: 'Unirest-Java' },
+        { url: 'https://github.com/Kong/unirest-java/pull/384', title: 'Removed redundant access modifiers, fixed typos and simplified some assertions', projectName: 'Unirest-Java' },
+        { url: 'https://github.com/eclipse-vertx/vert.x/pull/3719', title: 'Support for generic Key and Trust material', projectName: 'Vert.x' },
+        { url: 'https://github.com/com-lihaoyi/requests-scala/pull/62', title: 'Support for custom SSLContext for ssl configuration', projectName: 'Requests-Scala' },
+        { url: 'https://github.com/ktorio/ktor/pull/1852', title: 'Bug-fix: enhanced handling of statusCode for AndroidEngine', projectName: 'Ktor' },
+        { url: 'https://github.com/eclipse-ee4j/jersey/pull/4573', title: 'Support for SSL Configuration within JerseyTest', projectName: 'Jersey' },
+        { url: 'https://github.com/JetBrains/Exposed/pull/1852', title: 'Fix: Verbose logging in test module', projectName: 'Exposed' },
       ],
     },
   ]);
@@ -92,7 +91,6 @@ export default function OpenSourceSection() {
                 <Table>
                   <TableHeader>
                     <TableRow>
-                      <TableHead className="w-[100px]">PR</TableHead>
                       {contribution.showProjectNameInRows ? <TableHead className="w-[150px]">Project</TableHead>: null }
                       <TableHead>Title</TableHead>
                       <TableHead className="w-[120px]">Link</TableHead>
@@ -101,7 +99,6 @@ export default function OpenSourceSection() {
                   <TableBody>
                     {contribution.contributions.map((pr, prIndex) => (
                       <TableRow key={prIndex}>
-                        <TableCell className="font-medium">#{pr.prNumber}</TableCell>
                         {pr.projectName ? <TableCell>{pr.projectName}</TableCell>: null }
                         <TableCell>{pr.title || "Loading..."}</TableCell>
                         <TableCell>
